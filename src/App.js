60c65ec2e17a6bd13c96efd616b0d389ab60aee3
unknown <mahmoudelkhadragy38@gmail.com>
2020-10-17 03:11:02 +0200

    transmit folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Covid 19 tracker</div>;
+  return <div className="App">Covid r</div>;
 }
 
 export default App;
